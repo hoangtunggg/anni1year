53fcf38ba9eee6fb1743e73b173f5c90af103495
hoangtunggg <tungthkk1@gmail.com>
2024-11-04 11:42:51 +0700

    Lời nhắn commit đầu tiên

--- js/stars.js
+++ js/stars.js
@@ -1,209 +1,192 @@
-//import utils from "./utils"
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-
-canvas.width = innerWidth
-canvas.height = innerHeight
-
-window.addEventListener("resize", function() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;     
-    init()
-});
-
-/*
-const mouse = {
-    x: innerWidth / 2,
-    y: innerHeight / 2
-}
-
-
-// Event Listeners
-addEventListener('mousemove', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
-})
-*/
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 // Objects
-function Star(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+function Heart(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.size = radius;
     this.velocity = {
         x: (Math.random() - 0.5) * 8,
-        y: 3
-    }
-    this.friction = 0.8
-    this.gravity = 1
+        y: (Math.random() * 1) + 2
+    };
+    this.friction = 1;
+    this.gravity = 0.05;
 }
 
-Star.prototype.draw = function() {
-    c.save()
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.shadowColor = '#E3EAEF'
-    c.shadowBlur = 20
-    c.fill()
-    c.closePath()
-    c.restore()
-}
+Heart.prototype.draw = function() {
+    drawHeart(this.x, this.y, this.size);
+};
 
-Star.prototype.update = function() {
-    this.draw()
+Heart.prototype.update = function() {
+    this.draw();
 
-    //When ball hits bottom of screen
-    if(this.y + this.radius + this.velocity.y > canvas.height - groundHeight){
-        this.velocity.y = -this.velocity.y * this.friction
-        this.shatter()
-    }
-    else{
-        this.velocity.y += this.gravity
+    if (this.y + this.size + this.velocity.y > canvas.height - groundHeight) {
+        this.velocity.y = -this.velocity.y * this.friction;
+        this.y = canvas.height - groundHeight - this.size;
+        this.shatter();
+    } else {
+        this.velocity.y += this.gravity;
     }
 
-    //Hits side of screen
-    if(this.x + this.radius + this.velocity.x > canvas.width || this.x - this.radius <= 0){
-        this.velocity.x = -this.velocity.x * this.friction
-        this.shatter()
+    if (this.x + this.size + this.velocity.x > canvas.width) {
+        this.x = canvas.width - this.size;
+        this.velocity.x = -this.velocity.x * this.friction;
+        this.shatter();
+    } else if (this.x - this.size < 0) {
+        this.x = this.size;
+        this.velocity.x = -this.velocity.x * this.friction;
+        this.shatter();
     }
 
-    this.x += this.velocity.x
-    this.y += this.velocity.y
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+};
 
-}
-
-Star.prototype.shatter = function(){
-    this.radius -= 3
-    for(let i = 0; i < 8; i++){
-        miniStars.push(new MiniStar(this.x, this.y, 2))
+Heart.prototype.shatter = function() {
+    this.size -= 3;
+    for (let i = 0; i < 8; i++) {
+        miniHearts.push(new MiniHeart(this.x, this.y, 2));
     }
+};
 
-}
-
-function MiniStar(x, y, radius, color){
-    Star.call(this, x, y, radius, color)
+function MiniHeart(x, y, radius) {
+    Heart.call(this, x, y, radius);
     this.velocity = {
         x: (Math.random() - 0.5) * 10,
-        y: (Math.random() - 0.5) * 30 
-    }
-    this.friction = 0.8
-    this.gravity = 0.1
-    this.ttl = 100
-    this.opacity = 1
+        y: (Math.random() - 0.5) * 30
+    };
+    this.friction = 0.8;
+    this.gravity = 0.1;
+    this.ttl = 100;
+    this.opacity = 1;
 }
 
-MiniStar.prototype.draw = function() {
-    c.save()
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = `rgba(227,234, 239, ${this.opacity})`
-    c.shadowColor = '#E3EAEF'
-    c.shadowBlur = 20
-    c.fill()
-    c.closePath()
-    c.restore()
-}
+MiniHeart.prototype.draw = function() {
+    c.save();
+    c.globalAlpha = this.opacity;
+    drawHeart(this.x, this.y, this.size);
+    c.restore();
+};
 
-MiniStar.prototype.update = function() {
-    this.draw()
+MiniHeart.prototype.update = function() {
+    this.draw();
 
-    if(this.y + this.radius + this.velocity.y > canvas.height - groundHeight){
-        this.velocity.y = -this.velocity.y * this.friction
-    }
-    else{
-        this.velocity.y += this.gravity
+    if (this.y + this.size + this.velocity.y > canvas.height - groundHeight) {
+        this.velocity.y = -this.velocity.y * this.friction;
+    } else {
+        this.velocity.y += this.gravity;
     }
 
-    this.x += this.velocity.x
-    this.y += this.velocity.y
-    this.ttl -= 1
-    this.opacity -= 0.0001 * this.ttl
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.ttl -= 1;
+    this.opacity -= 0.01;
+};
+
+function drawHeart(x, y, size) {
+    c.save();
+    c.beginPath();
+    c.moveTo(x, y);
+    c.bezierCurveTo(x, y + size / 2, x - size, y + size / 2, x - size, y);
+    c.bezierCurveTo(x - size, y - size, x, y - size / 2, x, y - size);
+    c.bezierCurveTo(x, y - size / 2, x + size, y - size, x + size, y);
+    c.bezierCurveTo(x + size, y + size, x, y + size / 2, x, y);
+    c.fillStyle = 'red';
+    c.fill();
+    c.closePath();
+    c.restore();
 }
 
-function creatMountainRange(mountainAmount, height, color){
-    for(let i = 0; i < mountainAmount; i++){
-        const mountainWidth = canvas.width / mountainAmount
-        c.beginPath()
-        c.moveTo(i * mountainWidth, canvas.height)
-        c.lineTo(i * mountainWidth + mountainWidth + 0.2*canvas.height, canvas.height)
-        c.lineTo(i * mountainWidth + mountainWidth / 2, canvas.height - height)
-        c.lineTo(i * mountainWidth - 0.2*canvas.height, canvas.height)
-        c.fillStyle = color
-        c.fill()
-        c.closePath()
-    }
+function drawHeart2(x, y, size) {
+    c.save();
+    c.beginPath();
+    c.moveTo(x, y);
+
+    // Điều chỉnh các đường cong Bézier để tạo hình trái tim hoàn hảo
+    c.bezierCurveTo(x, y - size / 3, x - size / 2, y - size / 3, x - size / 2, y); // Bên trái
+    c.bezierCurveTo(x - size / 2, y + size / 2, x, y + size / 4, x, y + size / 2); // Dưới trái
+    c.bezierCurveTo(x, y + size / 4, x + size / 2, y + size / 2, x + size / 2, y); // Dưới phải
+    c.bezierCurveTo(x + size / 2, y - size / 3, x, y - size / 3, x, y); // Bên phải
+
+    c.fillStyle = '#A00000';// Đặt màu trái tim thành màu đỏ
+    c.fill(); // Đổ màu vào hình đã vẽ
+    c.closePath(); // Đóng đường vẽ
+    c.restore(); // Khôi phục trạng thái canvas trước đó
 }
 
-// Implementation
-const backgroundGradient = c.createLinearGradient(0, 0, canvas.width, canvas.height)
-backgroundGradient.addColorStop(0, '#171e26')
-backgroundGradient.addColorStop(1, '#3f586b')
 
-let stars
-let miniStars
-let backgroundStars
-let ticker = 0
-let randomSpawnRate = 75
-const groundHeight = 0.09 * canvas.height
-let inf = 1e9
+
+
+ 
+// Hàm vẽ một trái tim tĩnh duy nhất trong nền
+function drawStaticHeart() {
+    const x = canvas.width / 2;
+    const y = canvas.height / 2.5;
+    const size = 700; // Kích thước của trái tim
+    drawHeart2(x, y, size);
+}
+
+// Đổi màu background
+const backgroundGradient = c.createLinearGradient(0, 0, canvas.width, canvas.height);
+backgroundGradient.addColorStop(0, '#ff9a9e');
+backgroundGradient.addColorStop(1, '#fad0c4');
+
+let hearts = [];
+let miniHearts = [];
+let ticker = 0;
+let randomSpawnRate = 75;
+const groundHeight = 0.09 * canvas.height;
+let inf = 1e9;
+
 function init() {
-    stars = []
-    miniStars = []
-    backgroundStars = []
-   
-    for(let i = 0; i < 200; i++){
-        const x = Math.random() * canvas.width
-        const y = Math.random() * canvas.height
-        const radius = Math.random() * 3
-        backgroundStars.push(new Star(x, y, radius, 'white'))
+    hearts = [];
+    miniHearts = [];
+
+    for (let i = 0; i < 200; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height * 0.5;
+        const radius = Math.random() * 8 + 2;
+        hearts.push(new Heart(x, y, radius));
     }
 }
 
-// Animation Loop
 function animate() {
-    c.clearRect(0, 0, 0, canvas.height)
-    c.fillStyle = backgroundGradient
-    c.fillRect(0, 0, canvas.width, canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = backgroundGradient;
+    c.fillStyle = 'black'; // Đặt màu nền là đen
+    c.fillRect(0, 0, canvas.width, canvas.height); // Tô nền
 
-    backgroundStars.forEach(backgroundStar => {
-        backgroundStar.draw()
-    })
+    // Vẽ một trái tim tĩnh trong nền
+    drawStaticHeart();
 
-    if(flag) creatMountainRange(1, canvas.height * 0.7, '#384551')
-    if(flag) creatMountainRange(2, canvas.height * 0.6, '#2B3843')
-    if(flag) creatMountainRange(3, canvas.height * 0.4, '#26333E')
-    c.fillStyle = '#182028'
-    c.fillRect(0, canvas.height - groundHeight, canvas.width, groundHeight)
-    stars.forEach((star, index) => {
-        star.update();
-        if(star.radius == 0){
-            stars.splice(index, 1)
+    hearts.forEach(heart => {
+        heart.update();
+    });
+
+    miniHearts.forEach((miniHeart, index) => {
+        miniHeart.update();
+        if (miniHeart.ttl <= 0) {
+            miniHearts.splice(index, 1);
         }
     });
 
-    miniStars.forEach((miniStar, index) => {
-        miniStar.update();
-        if(miniStar.ttl == 0){
-            miniStars.splice(index, 1)
-        }
-    });
-
-    ticker++
-    if(ticker >= inf){
-        ticker = 0
+    ticker++;
+    if (ticker >= inf) {
+        ticker = 0;
     }
-    if(ticker % randomSpawnRate == 0){
-        const radius = 9
-        const x = Math.max(radius, Math.random() * canvas.width - radius)
-        stars.push(new Star(x, -100, 9, '#E3EAEF'))
-        randomSpawnRate = Math.floor(Math.random() * (200 - 125 + 1) + 125)
+    if (ticker % randomSpawnRate === 0) {
+        const radius = Math.random() * 8 + 2;
+        const x = Math.random() * canvas.width;
+        hearts.push(new Heart(x, -10, radius));
+        randomSpawnRate = Math.floor(Math.random() * (200 - 125 + 1) + 125);
     }
 
-    requestAnimationFrame(animate)
+    requestAnimationFrame(animate);
 }
 
-init()
-animate()
+init();
+animate();
--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,13 +1,11 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hé lu bé iu của toai!!!";
+let text2 = "Nay là 1 ngày đặc biệt nên ck muốn tặng vk iu 1 mon quà TINH THẦN nho nhỏ" 
+let text3 = "Mong là vk iu thích thích nó :> Iu vk nhìu lémmm" 
 let speed = 100;
 
 function typeWriter(text, para){
-	if(ok == 2){
-		clearInterval(typeInterval);
-	}
 	if(i < text.length){
 		document.getElementById(para).innerHTML += text.charAt(i);
 		i++;
@@ -21,16 +19,16 @@ function typeWriter(text, para){
 	}
 }
 
-var typeInterval;
+setInterval(function(){
+	if(ok == 0){
+		typeWriter(text1, "txt1");
+	}
+	else if(ok == 1){
+		typeWriter(text2, "txt2");
+	}
+	else if(ok == 2){
+        typeWriter(text3, "txt3");
+    }
+}, 100)
 
-//window.onload = function() {
-//	window.onload = function(){};
-   	typeInterval = setInterval(function(){
-		if(ok == 0){
-			typeWriter(text1, "txt1");
-		}
-		else if(ok == 1){
-			typeWriter(text2, "txt2");
-		}
-	}, 100);
-//};
+
